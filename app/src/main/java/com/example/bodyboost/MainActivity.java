e4c00e46d0172dd41cba1dee00440fdb6bbb65d4
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-06-28 17:53:40 +0600

    profile

--- app/src/main/java/com/example/bodyboost/MainActivity.java
+++ app/src/main/java/com/example/bodyboost/MainActivity.java
@@ -9,22 +9,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView tvName, tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,17 +33,6 @@ public class MainActivity extends AppCompatActivity {
             finish();
             return;
         }
-
-        tvName = findViewById(R.id.tv_name);
-        tvEmail = findViewById(R.id.tv_email1);
-
-        Button btnLogout = findViewById(R.id.btn_logOut);
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LogoutUser();
-            }
-        });
 
 //        here
         CardView findWater = findViewById(R.id.tv_water_card);
@@ -87,36 +68,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        here
-
-
-//        here
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User user = snapshot.getValue(User.class);
-                if (user != null){
-                    tvName.setText("Name: "  + user.name);
-                    tvEmail.setText("Email: " + user.email);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
     }
 
-    private void LogoutUser(){
-        FirebaseAuth.getInstance().signOut();
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
 
 //here bottom nav
 
--- app/src/main/java/com/example/bodyboost/ProfileActivity.java
+++ app/src/main/java/com/example/bodyboost/ProfileActivity.java
@@ -1,14 +1,81 @@
 package com.example.bodyboost;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView tvName, tvEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+        if (currentUser == null){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
+        tvName = findViewById(R.id.tv_name);
+        tvEmail = findViewById(R.id.tv_email1);
+
+        Button btnLogout = findViewById(R.id.btn_logOut);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LogoutUser();
+            }
+        });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("users").child(currentUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user = snapshot.getValue(User.class);
+                if (user != null){
+                    tvName.setText("Name: "  + user.name);
+                    tvEmail.setText("Email: " + user.email);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+    }
+
+    private void LogoutUser(){
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
