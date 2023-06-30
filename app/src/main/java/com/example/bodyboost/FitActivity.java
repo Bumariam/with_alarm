0c243f0131fbd38abce276f8bd7cb78fc6fd7694
R2D2 <r2d2@MacBook-Pro-R2D2.local>
2023-06-30 17:08:12 +0600

    30.06.23

--- app/src/main/java/com/example/bodyboost/FitActivity.java
+++ app/src/main/java/com/example/bodyboost/FitActivity.java
@@ -1,8 +1,14 @@
 package com.example.bodyboost;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class FitActivity extends AppCompatActivity {
 
@@ -10,5 +16,38 @@ public class FitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fit);
+        setupMenuNavigation();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_nav, menu);
+        return true;
+    }
+    private void setupMenuNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.fit) {
+                    // Открываем активити FitActivity
+                    startActivity(new Intent(FitActivity.this, FitActivity.class));
+                    return true;
+                }else if (id == R.id.home) {
+                    // Открываем активити HomeActivity
+                    startActivity(new Intent(FitActivity.this, MainActivity.class));
+                    return true;
+                }else if (id == R.id.profile) {
+                    // Открываем активити ProfileActivity
+                    startActivity(new Intent(FitActivity.this, ProfileActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bodyboost/ProfileActivity.java
+++ app/src/main/java/com/example/bodyboost/ProfileActivity.java
@@ -1,14 +1,17 @@
 package com.example.bodyboost;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -27,6 +30,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        setupMenuNavigation();
 
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -77,5 +81,37 @@ public class ProfileActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_nav, menu);
+        return true;
+    }
+    private void setupMenuNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.home) {
+                    // Открываем активити HomeActivity
+                    startActivity(new Intent(ProfileActivity.this, MainActivity.class));
+                    return true;
+                } else if (id == R.id.fit) {
+                    // Открываем активити FitActivity
+                    startActivity(new Intent(ProfileActivity.this, FitActivity.class));
+                    return true;
+                } else if (id == R.id.profile) {
+                    // Открываем активити ProfileActivity
+                    startActivity(new Intent(ProfileActivity.this, ProfileActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
