e1e32b0dcd6a3a150d251338e9ca69dc8f24347d
Нурулан <nurulan@MacBook-Pro-Ali.mshome.net>
2023-07-10 10:52:54 +0600

    FIT_aliya

--- app/src/main/java/com/example/bodyboost/ThirdActivity.java
+++ app/src/main/java/com/example/bodyboost/ThirdActivity.java
@@ -2,13 +2,63 @@ package com.example.bodyboost;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    String buttonvalue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+
+        Intent intent = getIntent();
+        buttonvalue = intent.getStringExtra("value");
+
+
+        int intvalue = Integer.valueOf(buttonvalue);
+
+
+        switch (intvalue){
+
+
+
+            case 1:
+                setContentView(R.layout.activity_bow);
+                break;
+            case 2:
+                setContentView(R.layout.activity_bridge);
+                break;
+            case 3:
+                setContentView(R.layout.activity_chair);
+                break;
+            case 4:
+                setContentView(R.layout.activity_child);
+                break;
+            case 5:
+                setContentView(R.layout.activity_cobbler);
+                break;
+            case 6:
+                setContentView(R.layout.activity_cow);
+                break;
+            case 7:
+                setContentView(R.layout.activity_playji);
+                break;
+            case 8:
+                setContentView(R.layout.activity_pauseji);
+                break;
+            case 9:
+                setContentView(R.layout.activity_plank);
+                break;
+            case 10:
+                setContentView(R.layout.activity_crunches);
+                break;
+            case 11:
+                setContentView(R.layout.activity_situp);
+                break;
+        }
     }
 }
